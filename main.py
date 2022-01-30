1818c234e116c95705ff03037f3e25338944dcb4
cozajeden <cozajeden@gmail.com>
2022-01-30 21:08:24 +0100

    initial

--- main.py
+++ main.py
@@ -7,12 +7,7 @@ xx = (np.sin(steps)+ np.sin(5*steps)+np.cos(3*steps))*150
 yy = (np.cos(steps)+ np.cos(11*steps)+np.sin(5*steps))*150
 for x0, y0 in zip(xx, yy):
     img = 200/np.sqrt((x)**2+(y)**2) + 100/np.sqrt((x-x0)**2+(y+y0)**2)
-    _, img = cv.threshold(
-        img[::-1,:],
-        2,
-        255,
-        cv.THRESH_BINARY
-    )
+    _, img = cv.threshold(img[::-1,:], 2, 255, cv.THRESH_BINARY)
     cv.imshow('img', img)
     cv.waitKey(40)
 cv.destroyAllWindows()
