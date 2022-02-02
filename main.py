5c91639a169fd58ef59bbf871a98a958f5afee61
cozajeden <cozajeden@gmail.com>
2022-02-02 21:30:18 +0100

    Made it interactive

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import cv2 as cv
 import numpy as np
 
-y, x = np.mgrid[:512, :512] - 256
-steps = np.linspace(0, np.pi, 200)
-xx = (np.sin(steps) + np.sin(5*steps) + np.cos(3*steps))*150
-yy = (np.cos(steps) + np.cos(11*steps) + np.sin(5*steps))*150
-for x0, y0 in zip(xx, yy):
-    img = 100/np.sqrt((x)**2+(y)**2) + 50/np.sqrt((x-x0)**2+(y+y0)**2)
-    _, img = cv.threshold(img[::-1,:], 1, 255, cv.THRESH_BINARY)
-    cv.imshow('img', img)
-    cv.waitKey(40)
+RESOLUTION = 800
+SHIFT = (RESOLUTION >> 1)
+Y, X = np.mgrid[:RESOLUTION, :RESOLUTION] - SHIFT
+MY = Y-SHIFT
+
+def move_circle(event, x, y, flags, param):
+    if event == cv.EVENT_MOUSEMOVE:
+        img = 100/np.sqrt((X)**2+(Y)**2) + 50/np.sqrt((X-x+SHIFT)**2+(MY+y)**2)
+        _, img = cv.threshold(img[::-1,:], 1, 255, cv.THRESH_BINARY)
+        img = cv.GaussianBlur(img, (3,3), 0, 0)
+        imgx = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=3, scale=1)
+        imgy = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=3, scale=1)
+        imgxy = cv.Sobel(img, cv.CV_16S, 1, 1, ksize=3, scale=1)
+        img = cv.addWeighted(imgx, 0.5, imgy, 0.5, 0)
+        img = cv.addWeighted(imgxy, 0.33, img, 0.66, 0)
+        img = cv.inRange(img, 0, 0)
+        img = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX)
+        cv.imshow('image', img)
+
+cv.namedWindow('image')
+cv.setMouseCallback('image', move_circle)
+move_circle(cv.EVENT_MOUSEMOVE, SHIFT, SHIFT, 0, 0)
+while True:
+    key = cv.waitKey(1)
+    if key == 27:
+        break
 cv.destroyAllWindows()
