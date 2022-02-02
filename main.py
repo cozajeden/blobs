f89c6263b28a6aa773cb065855c0c6644a718dce
cozajeden <cozajeden@gmail.com>
2022-02-02 21:51:15 +0100

    Version without filters

--- main.py
+++ main.py
@@ -4,21 +4,12 @@ import numpy as np
 RESOLUTION = 800
 SHIFT = (RESOLUTION >> 1)
 Y, X = np.mgrid[:RESOLUTION, :RESOLUTION] - SHIFT
-MY = Y-SHIFT
+MY = Y+SHIFT
 MX = X+SHIFT
 
 def move_circle(event, x, y, flags, param):
     if event == cv.EVENT_MOUSEMOVE:
-        img = 100/np.sqrt((X)**2+(Y)**2) + 50/np.sqrt((MX-x)**2+(MY+y)**2)
-        _, img = cv.threshold(img[::-1,:], 1, 255, cv.THRESH_BINARY)
-        img = cv.GaussianBlur(img, (3,3), 0, 0)
-        imgx = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=3, scale=1)
-        imgy = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=3, scale=1)
-        imgxy = cv.Sobel(img, cv.CV_16S, 1, 1, ksize=3, scale=1)
-        img = cv.addWeighted(imgx, 0.5, imgy, 0.5, 0)
-        img = cv.addWeighted(imgxy, 0.33, img, 0.66, 0)
-        img = cv.inRange(img, 0, 0)
-        img = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX)
+        img = 100/np.sqrt((X)**2+(Y)**2) + 50/np.sqrt((MX-x)**2+(MY-y)**2)
         cv.imshow('image', img)
 
 cv.namedWindow('image')
