eb9d8bf28c43a9bb347d4a3dcd827bac44f41e12
cozajeden <cozajeden@gmail.com>
2022-02-02 22:42:40 +0100

    close to 1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ MX = X+SHIFT
 def move_circle(event, x, y, flags, param):
     if event == cv.EVENT_MOUSEMOVE:
         img = 100/np.sqrt((X)**2+(Y)**2) + 50/np.sqrt((MX-x)**2+(MY-y)**2)
+        img = cv.inRange(img, 0.9, 1)
         cv.imshow('image', img)
 
 cv.namedWindow('image')
