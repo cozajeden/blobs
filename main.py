a562b060faca783e452fc09379001d4f7bbae227
cozajeden <cozajeden@gmail.com>
2022-02-03 16:34:26 +0100

    added left and right click

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ Y, X = np.mgrid[:RESOLUTION, :RESOLUTION] - SHIFT
 MY = Y+SHIFT
 MX = X+SHIFT
 
-def move_circle(event, x, y, flags, param):
-    if event == cv.EVENT_MOUSEMOVE:
-        img = 100/np.sqrt((X)**2+(Y)**2) + 50/np.sqrt((MX-x)**2+(MY-y)**2)
-        img = cv.inRange(img, 0.99, 1)
-        cv.imshow('image', img)
 
-cv.namedWindow('image')
-cv.setMouseCallback('image', move_circle)
-move_circle(cv.EVENT_MOUSEMOVE, SHIFT, SHIFT, 0, 0)
+class Mandelbrot:
+    rm = 50
+    def move_circle(self, event, x, y, flags, param):
+        if event == cv.EVENT_MOUSEMOVE:
+            img = 100/np.sqrt((X)**2+(Y)**2) + self.rm/np.sqrt((MX-x)**2+(MY-y)**2)
+            img = cv.inRange(img, 0.99, 1)
+            cv.imshow('image', img)
+        if event == cv.EVENT_LBUTTONDOWN:
+            self.rm += 10
+            self.move_circle(cv.EVENT_MOUSEMOVE, x, y, flags, param)
+        if event == cv.EVENT_RBUTTONDOWN:
+            self.rm -= 10
+            self.move_circle(cv.EVENT_MOUSEMOVE, x, y, flags, param)
+
+mandelbrot = Mandelbrot()
+
+cv.namedWindow('image', cv.WINDOW_GUI_NORMAL | cv.WINDOW_AUTOSIZE)
+cv.setMouseCallback('image', mandelbrot.move_circle)
+mandelbrot.move_circle(cv.EVENT_MOUSEMOVE, SHIFT, SHIFT, 0, 0)
 while True:
     key = cv.waitKey(1)
     if key == 27:
